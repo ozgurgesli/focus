edb7bc1283f5e08d801252d9e0ef84547332c7c6
ozgurgesli <443116+ozgurgesli@users.noreply.github.com>
2026-01-22 01:21:53 +0300

    added mqtt as a lib

--- main.c
+++ main.c
@@ -2,39 +2,70 @@
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
+#include <winsock2.h>
 #include <psapi.h>
 
+#include "mqtt.h"
+
+#pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "psapi.lib")
+
+#define BUF_SIZE 2048
 
 char mqtt_host[128];
 int  mqtt_port;
-char mqtt_user[64];
-char mqtt_pass[64];
 char mqtt_topic[128];
 int  interval_ms;
+
+SOCKET sock;
+struct mqtt_client client;
+uint8_t sendbuf[BUF_SIZE];
+uint8_t recvbuf[BUF_SIZE];
 
 void read_config() {
     GetPrivateProfileStringA("mqtt", "host", "", mqtt_host, 128, ".\\focus.ini");
     mqtt_port = GetPrivateProfileIntA("mqtt", "port", 1883, ".\\focus.ini");
-    GetPrivateProfileStringA("mqtt", "username", "", mqtt_user, 64, ".\\focus.ini");
-    GetPrivateProfileStringA("mqtt", "password", "", mqtt_pass, 64, ".\\focus.ini");
     GetPrivateProfileStringA("mqtt", "topic", "pc/activity/focus", mqtt_topic, 128, ".\\focus.ini");
     interval_ms = GetPrivateProfileIntA("general", "interval_ms", 1000, ".\\focus.ini");
 }
 
+void mqtt_connect() {
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2,2), &wsa);
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(mqtt_port);
+    addr.sin_addr.s_addr = inet_addr(mqtt_host);
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    connect(sock, (struct sockaddr*)&addr, sizeof(addr));
+
+    mqtt_init(&client, sock, sendbuf, BUF_SIZE, recvbuf, BUF_SIZE, NULL);
+
+    mqtt_connect(&client, "focus-client", NULL, NULL, 0, NULL, NULL, 0, 400);
+}
+
+void mqtt_publish_state(const char *msg) {
+    mqtt_publish(&client, mqtt_topic, msg, strlen(msg), MQTT_PUBLISH_QOS_0);
+}
+
 int main() {
     read_config();
+    mqtt_connect();
 
     char last_app[MAX_PATH] = "";
 
     while (1) {
+        mqtt_sync(&client);
+
         HWND hwnd = GetForegroundWindow();
         if (!hwnd) {
             Sleep(interval_ms);
             continue;
         }
 
-        DWORD pid = 0;
+        DWORD pid;
         GetWindowThreadProcessId(hwnd, &pid);
 
         HANDLE h = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_VM_READ, FALSE, pid);
@@ -52,14 +83,7 @@ int main() {
 
         if (strcmp(exe, last_app) != 0) {
             strcpy(last_app, exe);
-
-            char cmd[512];
-            sprintf(cmd,
-                "mosquitto_pub -h %s -p %d -t \"%s\" -m \"%s\"",
-                mqtt_host, mqtt_port, mqtt_topic, exe
-            );
-
-            system(cmd);
+            mqtt_publish_state(exe);
         }
 
         Sleep(interval_ms);
